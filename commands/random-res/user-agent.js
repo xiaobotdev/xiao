ee9759653b1874ad62076f50eb30b5207d9211de
Lily is Silly <lilyodendahl@gmail.com>
2025-07-20 01:09:41 -0400

    User Agent Command

---
+++ commands/random-res/user-agent.js
@@ -0,0 +1,17 @@
+const Command = require('../../framework/Command');
+const { UserAgent } = require('user-agents');
+
+module.exports = class UserAgentCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'user-agent',
+			group: 'random-res',
+			description: 'Responds with a random User Agent.'
+		});
+	}
+
+	run(msg) {
+		const agent = new UserAgent();
+		return msg.say(agent.toString());
+	}
+};
