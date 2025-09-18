e44b5536e6c9f0356b2b90e45b3d1bf445a04bb1
Lily is Silly <lilyodendahl@gmail.com>
2025-09-18 15:36:54 -0400

    Update undertale generator text location

--- commands/edit-image-text/undertale.js
+++ commands/edit-image-text/undertale.js
@@ -115,7 +115,7 @@ module.exports = class UndertaleCommand extends Command {
 		const text = wrapText(ctx, quote, 385);
 		const lines = text.length > 3 ? 3 : text.length;
 		for (let i = 0; i < lines; i++) {
-			ctx.fillText(text[i], 174, 22 + (22 * i) + (22 * i) + (space * i));
+			ctx.fillText(text[i], 174, 27 + (27 * i) + (27 * i) + (space * i));
 		}
 		return msg.say({ files: [{ attachment: canvas.toBuffer('image/png'), name: `undertale-${character}.png` }] });
 	}
